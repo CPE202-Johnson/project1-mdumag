09ef96f1327edbfaf5f22233f87e96bbd164c885
mdumag <77109688+mdumag@users.noreply.github.com>
2021-01-19 23:26:42 -0800

    Update perm_lex_testcases.py

--- perm_lex_testcases.py
+++ perm_lex_testcases.py
@@ -1,10 +1,13 @@
-import unittest
+iimport unittest
 import perm_lex
 
 class TestAssign1(unittest.TestCase):
 
     def test_perm_gen_lex(self):
         self.assertEqual(perm_lex.perm_gen_lex('ab'),['ab','ba'])
+    
+    def test_perm_gen_lex(self):
+        self.assertEqual(perm_lex.perm_gen_lex('abc'),['abc','acb','bac','bca','cab','cba'])
 
 if __name__ == "__main__":
         unittest.main()
